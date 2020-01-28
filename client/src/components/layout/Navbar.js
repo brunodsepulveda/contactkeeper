248626b9d1cb01ffbafb7a37e840e4fe7b38f438
Bruno Dias Sepulveda <brunodsepulveda@users.noreply.github.com>
2020-01-28 03:00:48 -0300

    Auth Context, Register and Login components

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,33 +1,41 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Navbar = ({ title, icon }) => {
   return (
     <div className="navbar bg-primary">
       <h1>
-        <Link to='/'><i className={icon} /> {title}</Link>
+        <Link to="/">
+          <i className={icon} /> {title}
+        </Link>
       </h1>
       <ul>
         <li>
-          <Link to='/'>Home</Link>
+          <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to='/about'>About</Link>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
         </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
 Navbar.propTypes = {
   title: PropTypes.string.isRequired,
   icon: PropTypes.string
-}
+};
 
 Navbar.defaultProps = {
-  title: 'Contact Keeper',
-  icon: 'fas fa-id-card-alt'
-}
+  title: "Contact Keeper",
+  icon: "fas fa-id-card-alt"
+};
 
 export default Navbar;
